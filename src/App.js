eb70df2efa2f60c1622d03c03810386b6a19bb6f
gitpratice <raajshrestha1999@gmial.com>
2023-12-24 21:18:41 +0545

    making navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import Home from "./pages/Home/Home"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import About from "./pages/About/About"
+import Projects from "./pages/Projects/Project"
+import Blog from "./pages/Blog/Blog"
+import Contact from "./pages/Contact/contact"
 import './App.css';
+import Education from "./pages/Education/Education"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <main className='main'>
+      <Home />
+      <Header />
+      <Footer />
+      <About />
+      <Projects />
+      <Education />
+      <Blog />
+      <Contact />
+    </main>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
-
+import "./Header.css"
 const navbar = () => {
   return (
     <>
-    <div className='container'>
-      <div className='navbar'>
-        <a href='#'>Home</a>
+    <nav className='nav_bar'>
+      <label className='logo'>Raj K. Shrestha</label>
+      <div className='nav_menu'>
+        <ul className='nav_list'>
+        <li className='nav_item'>
+        <a href='#home' className='nav_link'><span>H</span>ome</a>
+        </li>
+        <li className='nav_item'>
+        <a href='#home' className='nav_link'>About</a>
+        </li>
+        <li className='nav_item'>
+        <a href='#home' className='nav_link'>Blog</a>
+        </li>
+        <li className='nav_item'>
+        <a href='#home' className='nav_link'>Education</a>
+        </li>
+        <li className='nav_item'>
+        <a href='#home' className='nav_link'>Project</a>
+        </li>
+        <li className='nav_item'>
+        <a href='#home' className='nav_link'>Contact</a>
+        </li>
+        </ul>
       </div>
-    </div>
+    </nav>
     </>
   )
 }
